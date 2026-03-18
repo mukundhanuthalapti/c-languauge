17e60886d0bbb680b80cc26706d7dc5255646465
mukundha thrisummdhranni <nuthalapatitripura@gmail.com>
2026-03-18 10:27:03 +0530

    Create conitional statement prog3.c

---
+++ conitional statement prog3.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+int main()
+{
+    int score;
+    char name[20];
+    printf("Enter Player Name");
+    scanf("%s",name);
+    printf("Enter Score");
+    scanf("%d",&score);
+    if(score==0)
+    {
+        printf("%s Plays %d and he is  Dock Out",name,score);
+    }
+    else 
+    {
+        if(score>0 && score<50)
+        {
+        printf("%s Plays %d and he is  Below Half Century",name,score);
+        }
+        else
+        {
+            if(score==50)
+            {
+            printf("%s Plays %d and he is  Half Century",name,score);
+            }
+            else
+            {
+                if(score>50 && score<100)
+                {
+                printf("%s Plays %d and he is  Above Half Century",name,score);
+                }
+                    else
+                    {
+                    if(score==100)
+                    {
+                    printf("%s Plays %d and he is  Century",name,score);
+                    }
+                 
+                }
+            }
+        }
+    }
+            
+    return 0;
+}
+
+            
